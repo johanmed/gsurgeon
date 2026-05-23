a1abeefd4527cb2e57c202296bf52cb9ef0b4502
Johannes Medagbe <johanmedagbe@gmail.com>
2026-05-24 00:36:12 +0300

    New: add script to run gsurgeon with bootstrap on set of queries

---
+++ scripts/run_set.py
@@ -0,0 +1,70 @@
+"""
+Script to run gsurgeon with bootstrap on all questions in a set
+Author: Johannes Medagbe
+Copyright (c) 2026
+"""
+
+import argparse
+import asyncio
+import json
+import os
+
+import dspy
+from Bio import Entrez
+from dotenv import load_dotenv
+from gsurgeon.operations.standard import reoperate
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path", help="Path to file with set of questions")
+    parser.add_argument("--output-path", help="Path to output file")
+    parser.add_argument("--env-file", default=".env", help="Path to .env file")
+    args = parser.parse_args()
+
+    load_dotenv(dotenv_path=args.env_file)
+
+    EMAIL = os.getenv("EMAIL")
+    if EMAIL is None:
+        raise ValueError("Set EMAIL for NCBI tool calling")
+    Entrez.email = EMAIL
+
+    MODEL_NAME = os.getenv("MODEL_NAME")
+    if MODEL_NAME is None:
+        raise ValueError("Set MODEL_NAME of a provider")
+
+    API_KEY = os.getenv("API_KEY")
+    if API_KEY is None:
+        raise ValueError("Set valid API_KEY for proprietary model")
+    model = dspy.LM(
+        MODEL_NAME,
+        api_key=API_KEY,
+        max_tokens=10_000,
+        temperature=0,
+        verbose=False,
+    )
+
+    dspy.configure(lm=model)
+
+    N_ITERATIONS = os.getenv("N_ITERATIONS")
+    if N_ITERATIONS is None:
+        raise ValueError("Set N_ITERATIONS for operation")
+    n_iterations = int(N_ITERATIONS)
+
+    N_BOOTSTRAPS = os.getenv("N_BOOTSTRAPS")
+    if N_BOOTSTRAPS is None:
+        raise ValueError("Set N_BOOTSTRAPS for multiple runs")
+    n_bootstraps = int(N_BOOTSTRAPS)
+
+    with open(args.input_path) as i:
+        data = i.read()
+        questions = data.split("\n")
+
+    collection = {}
+    for ind, question in enumerate(questions):
+        response = asyncio.run(reoperate(question, n_iterations, n_bootstraps))
+        collection[f"Question {ind} was '{question}'"] = (
+            f"Response {ind} was '{response}'."
+        )
+
+    with open(args.output_path, "w") as o:
+        o.write(json.dumps(collection, indent=4))
