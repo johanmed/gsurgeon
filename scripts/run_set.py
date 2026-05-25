900ba2a818ff46530d31b691ec78d1f3846461a5
Johannes Medagbe <johanmedagbe@gmail.com>
2026-05-25 14:57:00 +0300

    New: add print statements to run of set questions

--- scripts/run_set.py
+++ scripts/run_set.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
         raise ValueError("Set N_BOOTSTRAPS for multiple runs")
     n_bootstraps = int(N_BOOTSTRAPS)
 
+    print("Running operation for a set of questions...")
     with open(args.input_path) as i:
         data = i.read()
         questions = data.split("\n")
@@ -68,3 +69,4 @@ if __name__ == "__main__":
 
     with open(args.output_path, "w") as o:
         o.write(json.dumps(collection, indent=4))
+    print("Run complete for all questions")
