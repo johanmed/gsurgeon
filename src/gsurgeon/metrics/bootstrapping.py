4daab0574cf4753f5ffa759ac1cdd26f2b5e193a
Johannes Medagbe <johanmedagbe@gmail.com>
2026-06-15 12:26:20 +0300

    Format: format to PEP bootstrapping.py

--- src/gsurgeon/metrics/bootstrapping.py
+++ src/gsurgeon/metrics/bootstrapping.py
@@ -4,8 +4,7 @@ from collections import Counter
 
 
 def bootstrap(
-    element_ranks: dict[str, list[int]]
-    descend: bool = False
+    element_ranks: dict[str, list[int]], descend: bool = False
 ) -> dict[tuple[str, int], tuple[float, int]]:
     """
     Compute bootstrap rank for each element
@@ -23,5 +22,7 @@ def bootstrap(
         )[0]
         top_rank, frequency = top
         bootstrap_ranks[(element, top_rank)] = (frequency / total, total)
-    bootstrap_ranks = dict(sorted(bootstrap_ranks.items(), key=lambda item: item[0][1], reverse=descend))
+    bootstrap_ranks = dict(
+        sorted(bootstrap_ranks.items(), key=lambda item: item[0][1], reverse=descend)
+    )
     return bootstrap_ranks
